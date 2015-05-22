26ae202c39e72db35bfd0f2f560be83a24b1e543
nlangloi <nmonette@hotmail.ca>
2015-05-22 08:13:00 -0400

    Ajout gitignore

--- script/main.js
+++ script/main.js
@@ -1,20 +1,20 @@
-video_play.addEventListener('change', function(){
-    if (this.checked){
+video_play.addEventListener('change', function () {
+    if (this.checked) {
         video.play();
-    }else{
+    } else {
         video.pause();
     }
 
 });
-listteam.addEventListener('change', function(){
+listteam.addEventListener('change', function () {
     var number_2 = document.getElementById('number2');
-    if (listteam.value == 'Tampa Bay Lightning'){
+    if (listteam.value == 'Tampa Bay Lightning') {
         number_2.value = 1.25;
-    }else if(listteam.value == 'Chicago Blackhawks'){
+    } else if (listteam.value == 'Chicago Blackhawks') {
         number_2.value = 6;
-    }else if(listteam.value == 'Anaheim Ducks'){
+    } else if (listteam.value == 'Anaheim Ducks') {
         number_2.value = 5;
-    }else if(listteam.value == 'New York Rangers'){
+    } else if (listteam.value == 'New York Rangers') {
         number_2.value = 1.5;
     }
 });
